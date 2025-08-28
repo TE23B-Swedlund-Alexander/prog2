81284f2644a604ff1b05392c8b9bf8e0be5e6b65
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-08-28 12:57:16 +0200

    sdfghj

--- repitititititititition/Program.cs
+++ repitititititititition/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 int hp1 = 100;
 int hp2 = 100;
-
-string name1 = "sAlex";
+int dmg1;
+int dmg2;
+Console.WriteLine("what is your name?");
+string name1 = Console.ReadLine();
 string name2 = "bAlex";
 
 while (hp1 > 0 && hp2 > 0)
@@ -10,9 +12,27 @@ while (hp1 > 0 && hp2 > 0)
 
     Console.WriteLine($"{name1} hp:{hp1}");
     Console.WriteLine($"{name2} hp:{hp2}");
-    hp2 -= Random.Shared.Next(10, 25);
-    hp1 -= Random.Shared.Next(10, 25);
+    dmg1 = Random.Shared.Next(10, 25);
+    dmg2 = Random.Shared.Next(10, 25);
+    hp2 -= dmg1
+    hp1 -= dmg2
+    Console.WriteLine($"{name1} did {dmg1} damage");
+    Console.WriteLine($"{name2} did {dmg2} damage");
     Console.ReadLine();
-Console.WriteLine("mega victreebel is Awesome");
+    Console.WriteLine("mega victreebel is Awesome");
+    Console.ReadLine();
 }
-Console.ReadLine();
+if (hp1 > 0 && hp2 < 1)
+{
+    Console.WriteLine("you win");
+
+}
+else if (hp1 < 1 && hp2 > 0)
+{
+    Console.WriteLine("you lose");
+}
+else
+{
+    Console.WriteLine("fly");
+}
+    Console.ReadLine();
