c3f83f8512e29ce7e4130da427229723d57e002f
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-08-28 12:10:10 +0200

    jkbj

---
+++ repitititititititition/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int hp1 = 100;
+int hp2 = 100;
+
+string name1 = "sAlex";
+string name2 = "bAlex";
+
+while (hp1 > 0 && hp2 > 0)
+{
+
+    Console.WriteLine($"{name1} hp:{hp1}");
+    Console.WriteLine($"{name2} hp:{hp2}");
+    hp2 -= Random.Shared.Next(10, 25);
+    hp1 -= Random.Shared.Next(10, 25);
+    Console.ReadLine();
+Console.WriteLine("mega victreebel is Awesome");
+}
+Console.ReadLine();
