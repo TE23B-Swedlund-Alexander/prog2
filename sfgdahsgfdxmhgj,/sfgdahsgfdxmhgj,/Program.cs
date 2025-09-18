b14dfec24b67d8cc476a2fc0e4899e99fa3001a0
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-09-18 12:57:29 +0200

    afz

--- sfgdahsgfdxmhgj,/sfgdahsgfdxmhgj,/Program.cs
+++ sfgdahsgfdxmhgj,/sfgdahsgfdxmhgj,/Program.cs
@@ -11,4 +11,15 @@
 """);
     Console.ReadLine();
 }
+string[] word = ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"];
+int a = 11;
+void TypeWriter ()
+{
+    for (int i = 0; i < a; i++)
+    {
+        Console.Write(word[i]);
+        Console.ReadLine();
+    }
+}
 Window();
+TypeWriter();
