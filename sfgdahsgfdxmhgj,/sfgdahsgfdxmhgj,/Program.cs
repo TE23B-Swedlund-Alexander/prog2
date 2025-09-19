ce6db2408ab4cf949a42ae53ac3025628046fa5f
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2025-09-19 13:00:33 +0200

    fasfaef

--- sfgdahsgfdxmhgj,/sfgdahsgfdxmhgj,/Program.cs
+++ sfgdahsgfdxmhgj,/sfgdahsgfdxmhgj,/Program.cs
@@ -1,4 +1,6 @@
-﻿void Window()
+﻿using System.Linq.Expressions;
+
+void Window()
 {
 
 
@@ -11,15 +13,47 @@
 """);
     Console.ReadLine();
 }
-string[] word = ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"];
-int a = 11;
-void TypeWriter ()
+void TypeWriter()
 {
-    for (int i = 0; i < a; i++)
+string[] word = ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"];
+    int a = 11;
+    for (int i = a; i < a; i++)
     {
         Console.Write(word[i]);
-        Console.ReadLine();
+        Thread.Sleep(200);
     }
 }
-Window();
+static void Star()
+{
+    Console.SetCursorPosition(Random.Shared.Next(120), Random.Shared.Next(29));
+    Console.WriteLine("*");
+}
+
+static void Stars(int a)
+{
+    for (int i = a; i > 0; i--)
+    {
+        Star();
+    }
+}
+bool gotNumber = false;
+int numberYay;
+Console.WriteLine("give number");
+while (gotNumber == false)
+{
+
+    string numberMabye = Console.ReadLine();
+    bool isNumber = int.TryParse(numberMabye, out numberYay);
+    if (numberYay < -1 && isNumber == true)
+    {
+        
+     gotNumber = true;
+    }
+    
+}
+
+//Window();
 TypeWriter();
+Console.ReadLine();
+Stars(3232);
+Console.ReadLine();
